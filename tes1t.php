c3a4f72110e991d7e0ec533f56aa02b1023a31b4
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2023-01-15 01:08:51 +0330

    Update and rename test.php to tes1t.php

--- tes1t.php
+++ tes1t.php
@@ -2,3 +2,6 @@ To join the meeting on Google Meet, click this link:
 https://meet.google.com/uob-miek-nap 
  
 Or open Meet and enter this code: uob-miek-nap
+
+https://docs.google.com/document/d/1fsnHvd2vIn-yamBUezNsxfe8ibvNkVWv/edit?usp=drivesdk&ouid=107377437877782121862&rtpof=true&sd=true
+

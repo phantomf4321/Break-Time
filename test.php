3768c8703cdc4b61014110a6791b3bd1343ffaef
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2023-01-15 12:07:44 +0330

    Update test.php

--- test.php
+++ test.php
@@ -6,3 +6,8 @@ Or open Meet and enter this code: uob-miek-nap
 https://docs.google.com/document/d/1fsnHvd2vIn-yamBUezNsxfe8ibvNkVWv/edit?usp=drivesdk&ouid=107377437877782121862&rtpof=true&sd=true
 
 https://drive.google.com/file/d/1mpH9oNMwmrYH5phQ1NIGR4rdWzx-zTYY/view?usp=drivesdk
+
+
+https://neshan.org/maps/@35.706970,51.431867,15.0z,0.0p
+
+https://neshan.org/maps/@36.322384,59.547502,15.0z,0.0p/places/rb1M1aPJFJi2

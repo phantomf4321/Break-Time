21aab66df663fdc57e5d1e2f4caeb9a539487e8f
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2023-07-04 23:26:27 +0330

    Update admin_class.php

--- Baran Bayat/admin_class.php
+++ Baran Bayat/admin_class.php
@@ -1,3 +1,4 @@
+<?php
 class DB{
     private $host;
     private $username;
@@ -27,3 +28,49 @@ class DB{
         }
     }
 }
+class Visitor{
+
+    function login($username, $password){
+        // Create connection
+        $db = new DB;
+        $conn = $db->connect();
+        // Check connection
+        if ($conn == 0) {
+            return(101);
+        }else{
+            
+            session_start();
+            $uname = $username;
+        	$pass = $password;
+        	$pass = hash('sha512', $pass);
+        	if (empty($uname)) {
+        		return 0;
+        	    exit();
+        	}else if(empty($pass)){
+                return 2;
+        	    exit();
+        	}else{
+        		$sql = "SELECT * FROM visitors WHERE username='$uname' AND password='$pass'";
+        		$result = mysqli_query($conn, $sql);
+        		if (mysqli_num_rows($result) === 1) {
+        			$row = mysqli_fetch_assoc($result);
+                    if ($row['phone'] === $uname && $row['password'] === $pass) {
+                    	$_SESSION['username'] = $row['phone'];
+                    	$_SESSION['name'] = $row['name'];
+                    	$_SESSION['id'] = $row['id'];
+                    	return 1;
+        		        exit();
+                    }else{
+        				return 3;
+        		        exit();
+        			}
+        		}else{
+        			return 4;
+        	        exit();
+        		}
+        	}
+    	
+        }
+    }// end login...
+}
+?>

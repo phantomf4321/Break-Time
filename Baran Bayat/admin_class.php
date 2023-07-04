06777db7974adfa5ba26251503824bbc0e6a2383
Sajjad Ranjbar <sajjadranjbaryazdi@gmail.com>
2023-07-04 23:25:18 +0330

    Create admin_class.php

---
+++ Baran Bayat/admin_class.php
@@ -0,0 +1,29 @@
+class DB{
+    private $host;
+    private $username;
+    private $password;
+    private $db;
+    
+    function set(){
+        $this->host = "localhost";
+        $this->username = "root";
+        $this->password = "";
+        $this->db = "seminaram_nobat";
+    }
+    
+    function connect(){
+        $this->set();
+        $host = $this->host;
+        $username = $this->username;
+        $password = $this->password;
+        $db = $this->db;
+        
+        $conn = new mysqli($host, $username, $password, $db);
+        
+        if ($conn->connect_error) {
+            return(0);//failed
+        }else{
+            return($conn);//success
+        }
+    }
+}
